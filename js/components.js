b976c9e048f44630e5d7fecfa1c1dde3a1894f55
Philipp Benner <ppbenner@outlook.de>
2026-04-12 11:45:02 +0200

    seo optimization, ui updates

--- js/components.js
+++ js/components.js
@@ -1,83 +1,5 @@
-// Web Components for Header and Footer
+// Web Component for Footer
 // Used across all pages for consistency
-
-class SiteHeader extends HTMLElement {
-    connectedCallback() {
-        // Check if we're on the homepage for smooth scrolling behavior
-        const isHomepage = window.location.pathname === '/' ||
-            window.location.pathname === '/index.html' ||
-            window.location.pathname.endsWith('/index.html');
-
-        // On homepage: use data-href for smooth scroll, on other pages: use direct links
-        const loginLink = isHomepage ? 'href="#" class="menu-link" data-href="#section-services"' : 'href="https://app.propinsight.de" class="menu-link"';
-        const funktionenLink = isHomepage ? 'href="#" class="menu-link" data-href="#section-services"' : 'href="index.html#section-services" class="menu-link"';
-        const preiseLink = isHomepage ? 'href="#" class="menu-link" data-href="#section-pricing"' : 'href="index.html#section-pricing" class="menu-link"';
-        const faqsLink = isHomepage ? 'href="#" class="menu-link" data-href="#section-faqs"' : 'href="index.html#section-faqs" class="menu-link"';
-
-        this.innerHTML = `
-    <header id="header" class="transparent-header">
-        <div id="header-wrap">
-            <div class="container">
-                <div class="header-row">
-
-                    <!-- Logo -->
-                    <div id="logo" class="col-lg-2 me-lg-auto">
-                        <a href="index.html" class="w-auto">
-                            <img class="logo-default" src="demos/saas-2/images/logo.webp" alt="propinsight Logo">
-                            <img class="logo-sticky" src="demos/saas-2/images/logo-dark.webp" alt="propinsight Logo">
-                        </a>
-                    </div>
-
-                    <div class="header-misc ms-auto col-lg-2 justify-content-lg-end">
-                        <a href="https://app.propinsight.de" class="button button-border button-rounded rounded border-contrast-500 mx-0"
-                           style="--cnvs-btn-fontsize: 1.125rem"><i
-                                class="bi-person-fill d-none d-md-inline-block me-2 fs-6"></i>Login</a>
-                    </div>
-
-                    <div class="primary-menu-trigger">
-                        <button class="cnvs-hamburger" type="button" title="Open Mobile Menu">
-                            <span class="cnvs-hamburger-box"><span class="cnvs-hamburger-inner"></span></span>
-                        </button>
-                    </div>
-
-                    <!-- Primary Navigation -->
-                    <nav class="primary-menu with-arrows">
-
-                        <ul class="one-page-menu menu-container" data-easing="easeInOutExpo" data-speed="1250"
-                            data-offset="65">
-                            <li class="menu-item">
- <a ${loginLink}>                                    <div>Login</div>
-                                </a>
-                            </li>
-                            <li class="menu-item">
-                                <a ${funktionenLink}>
-                                    <div>Funktionen</div>
-                                </a>
-                            </li>
-
-                            <li class="menu-item">
-                                <a ${preiseLink}>
-                                    <div>Preise</div>
-                                </a>
-                            </li>
-                            <li class="menu-item">
-                                <a ${faqsLink}>
-                                    <div>FAQs</div>
-                                </a>
-                            </li>
-
-                        </ul>
-
-                    </nav>
-
-                </div>
-            </div>
-        </div>
-        <div class="header-wrap-clone"></div>
-    </header>
-        `;
-    }
-}
 
 class SiteFooter extends HTMLElement {
     connectedCallback() {
@@ -146,6 +68,5 @@ class SiteFooter extends HTMLElement {
     }
 }
 
-// Register the custom elements
-customElements.define('site-header', SiteHeader);
+// Register the custom element
 customElements.define('site-footer', SiteFooter);
